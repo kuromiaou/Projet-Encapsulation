512af58d796bfae50df1ccda6a07b672ff39b6cf
Mathieu . <cedroll333@gmail.com>
2025-01-17 09:25:31 +0100

    Add project files.

---
+++ Entity.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace sf;
+
+class Entity
+{
+};
+
---
+++ Projet Encapsulation 1.cpp
@@ -0,0 +1,11 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
